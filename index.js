ebf4910b7e4e858c46d4ac51b07f87bff6a6f64a
DaveMacLellan <davemaclellanx@gmail.com>
2022-05-10 00:23:32 -0400

    First Init
    Created first html page with some styling.
    
    Have search button working. Type a title of a movie and it will display a list of results matching word.

---
+++ index.js
@@ -0,0 +1,38 @@
+const searchText = document.getElementById("search-text")
+const results = document.getElementById("results")
+
+document.getElementById("findMovie").addEventListener("click", getMovieList)
+
+function getMovieList() {
+    results.innerHTML = ``
+    fetch(`http://www.omdbapi.com/?apikey=7bb14cc&s=${searchText.value}`)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        for(let i = 0; i < data.Search.length; i++){
+            fetch(`http://www.omdbapi.com/?apikey=7bb14cc&i=${data.Search[i].imdbID}`)
+            .then(res => res.json())
+            .then(data => {
+                //console.log(data)
+                results.innerHTML += `
+                    <div class="movie">
+                        <div class="movie-poster">
+                            <img class="poster" src="${data.Poster}">
+                        </div>
+                        <div class="movie-main">
+                            <h3 class="movie-title">${data.Title}</h3>
+                            <div class="movie-info">
+                                <p>${data.Runtime}</p>
+                                <p>${data.Genre}</p>
+                                <button>Watchlist</button>
+                            </div>
+                            <div class="movie-plot">
+                                <p>${data.Plot}</p>
+                            </div>
+                        </div>
+                    </div> 
+                `
+            })
+        }        
+    })
+}
