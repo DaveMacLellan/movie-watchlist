b6df46cf752e60f55e504171d94566804a1fc6da
DaveMacLellan <davemaclellanx@gmail.com>
2022-05-13 01:09:56 -0400

    Lots of changes to js
    Fixed local storage issue.
    Add items to local storage on button click.
    
    Stored items dont show up in searches. They show up in current search but button to add is removed.
    
    Added watchlist page.

--- index.js
+++ index.js
@@ -1,38 +1,88 @@
 const searchText = document.getElementById("search-text")
 const results = document.getElementById("results")
 
+let watchBtns = ""
+let watchlist = []
+let currentWatchlist = JSON.parse(localStorage.getItem("allMovies"));
+
 document.getElementById("findMovie").addEventListener("click", getMovieList)
 
-function getMovieList() {
+function getMovieList() {    
     results.innerHTML = ``
     fetch(`https://www.omdbapi.com/?apikey=7bb14cc&s=${searchText.value}`)
     .then(res => res.json())
     .then(data => {
-        console.log(data)
-        for(let i = 0; i < data.Search.length; i++){
-            fetch(`https://www.omdbapi.com/?apikey=7bb14cc&i=${data.Search[i].imdbID}`)
-            .then(res => res.json())
-            .then(data => {
-                //console.log(data)
-                results.innerHTML += `
-                    <div class="movie">
-                        <div class="movie-poster">
-                            <img class="poster" src="${data.Poster}">
-                        </div>
-                        <div class="movie-main">
-                            <h3 class="movie-title">${data.Title}</h3>
-                            <div class="movie-info">
-                                <p>${data.Runtime}</p>
-                                <p>${data.Genre}</p>
-                                <button>Watchlist</button>
-                            </div>
-                            <div class="movie-plot">
-                                <p>${data.Plot}</p>
-                            </div>
-                        </div>
-                    </div> 
-                `
-            })
-        }        
-    })
+        if(data.Response === "True"){
+            for(let movie of data.Search){
+                fetch(`https://www.omdbapi.com/?apikey=7bb14cc&i=${movie.imdbID}`)
+                .then(res => res.json())
+                .then(data => {
+                    const {imdbID} = data
+                    if(currentWatchlist === null){
+                        results.innerHTML += addHtml(data)
+                    }
+                    else {
+                        if(!currentWatchlist.includes(imdbID)){
+                            results.innerHTML += addHtml(data)
+                        }                     
+                    }                    
+                    
+                    watchBtns = document.querySelectorAll(".addMovie")
+                    watchBtns.forEach(button => {
+                        button.addEventListener("click", ()=> {
+                            fetch(`https://www.omdbapi.com/?apikey=7bb14cc&i=${button.id}`)
+                                    .then(res => res.json())
+                                    .then(data => {
+                                        const {imdbID} = data
+                                        if(currentWatchlist === null){
+                                            currentWatchlist = []
+                                            pushData(imdbID, button)
+                                        }
+                                        else {
+                                            if(!currentWatchlist.includes(imdbID)){
+                                                pushData(imdbID, button)                                                
+                                            }          
+                                        }  
+                            })
+                        })
+                    })
+
+                })
+            }      
+        }
+        else {
+            results.innerHTML += `
+                <div class="default-page">
+                        <p>No Match</p>
+                </div>  
+            `
+        }
+    })    
+}
+
+function addHtml(movie) {
+    return `
+        <div class="movie">
+            <div class="movie-poster">
+                <img class="poster" src="${movie.Poster}">
+            </div>
+            <div class="movie-main">
+                <h3 class="movie-title">${movie.Title}</h3>
+                <div class="movie-info">
+                    <p>${movie.Runtime}</p>
+                    <p>${movie.Genre}</p>
+                    <button class="addMovie" id="${movie.imdbID}">Watchlist</button>
+                </div>
+                <div class="movie-plot">
+                    <p>${movie.Plot}</p>
+                </div>
+            </div>
+        </div> 
+    `
+}
+
+function pushData(id, btn){
+    currentWatchlist.push(id);
+    localStorage.setItem("allMovies", JSON.stringify(currentWatchlist));
+    btn.style.display = "none"
 }
---
+++ watchlist.js
@@ -0,0 +1,33 @@
+const results = document.getElementById("results")
+
+function loopLocal(){
+    let currentWatchlist = JSON.parse(localStorage.getItem("allMovies"));
+    results.innerHTML = ``
+    for (var i = 0; i < currentWatchlist.length; i++) {
+        fetch(`https://www.omdbapi.com/?apikey=7bb14cc&i=${currentWatchlist[i]}`)
+        .then(res => res.json())
+        .then(data => {
+            const {Title, Runtime, Plot, Poster, Genre, imdbID} = data
+
+            results.innerHTML += `
+            <div class="movie">
+                <div class="movie-poster">
+                    <img class="poster" src="${Poster}">
+                </div>
+                <div class="movie-main">
+                    <h3 class="movie-title">${Title}</h3>
+                    <div class="movie-info">
+                        <p>${Runtime}</p>
+                        <p>${Genre}</p>
+                        <button class="removeMovie" id="${imdbID}">Remove</button>
+                    </div>
+                    <div class="movie-plot">
+                        <p>${Plot}</p>
+                    </div>
+                </div>
+            </div> 
+        `
+        })
+    }
+}
+loopLocal()
